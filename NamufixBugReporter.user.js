441a801a3b1c52649a3cd730d157745f45cdc6fc
nekopoly <spinpo12@naver.com>
2016-01-29 18:05:44 +0900

    img

--- NamufixBugReporter.user.js
+++ NamufixBugReporter.user.js
@@ -9,7 +9,7 @@
 // @namespace   http://nekopoly.n-e.kr/
 // @downloadURL https://github.com/Nekopoly/NamufixBugReporter/raw/master/NamufixBugReporter.user.js
 // @require     http://shironeko.nekopoly.n-e.kr/static/jquery-2.2.0.min.js
-// @version     b5
+// @version     b6
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
@@ -26,7 +26,7 @@
   if (node >= 7) {
     console.log('나무픽스(이)가 있습니다.');
     run();
-  } else {
+  } else if($('nf-donation').length <= 1)){
     //console.log('나무픽스(이)가 없습니다.');
         $('.container-fluid.wiki-article').prepend('<div class="alert alert-danger" role="alert" id="nbr_notice"><strong>[오류!]</strong><br>Namufix Bug Reporter 플러그인은 나무픽스가 없으면 동작하지않습니다!</div>');
         setTimeout(function () {
@@ -34,6 +34,8 @@
             //Stuff to do *after* the animation takes place
           });
         }, 4000);
+  }else{
+
   }
 });
 
@@ -41,8 +43,9 @@ function run() {
   var NEwindow_create='<div id="nbr_ne_window" style="background: rgba(0, 0, 0, 0.5) none repeat scroll 0% 0%; z-index: 999999; position: fixed; left: 0px; top: 0px; height: 100%; width: 100%;">'
   +'<div class="TooSimplePopup"><div class="header" style="background:#2D4996">나무픽스 버그리포터</div><div class="container">'
   +'<p>나무픽스를 이용하는도중 버그가발생하였나요?</p>'
+  +'<p>사진업로드는 imgur.com을 이용해주시길 바랍니다.</p>'
   +'<span>제목</span><input name="title" type="text" style="width:100%" />'
-  +'<span>내용</span><textarea id="nfCodeToBeautify" style="max-width: 100vw; max-height: calc(100vh - 150px); width: 400px; height: 390px; display: block;"></textarea>'
+  +'<span>내용</span><button type="button" style="background:#2D4996" id="nbr_img">이미지업로드</button><textarea id="nfCodeToBeautify" style="max-width: 100vw; max-height: calc(100vh - 150px); width: 400px; height: 390px; display: block;"></textarea>'
   +'</div><div class="footer"><button type="button" style="background:#2D4996" id="nbr_close">닫기</button></div></div></div>';
   $('.container-fluid.wiki-article').append(NEwindow_create);
   $('#nbr_close').click(function(event) {
