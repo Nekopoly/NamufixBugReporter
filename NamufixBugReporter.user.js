519166e8d1b1f2f61389283ff871b3fcfb33198e
nekopoly <spinpo12@naver.com>
2016-01-30 08:13:43 +0900

    json

--- NamufixBugReporter.user.js
+++ NamufixBugReporter.user.js
@@ -8,7 +8,7 @@
 // @include     http://issue.namu.wiki/*
 // @namespace   http://nekopoly.n-e.kr/
 // @downloadURL https://github.com/Nekopoly/NamufixBugReporter/raw/master/NamufixBugReporter.user.js
-// @version     b12
+// @version     b13
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
@@ -33,10 +33,13 @@
 function run() {
   var NEwindow_create='<div id="nbr_ne_window" style="background: rgba(0, 0, 0, 0.5) none repeat scroll 0% 0%; z-index: 999999; position: fixed; left: 0px; top: 0px; height: 100%; width: 100%;">'
   +'<div class="TooSimplePopup"><div class="header" style="background:#2D4996">나무픽스 버그리포터</div><div class="container">'
-  +'<p>나무픽스를 이용하는도중 버그가발생하였나요?</p>'
-  +'<p>사진업로드는 imgur.com을 이용해주시길 바랍니다.</p>'
-  +'<span>제목</span><input name="title" type="text" style="width:100%" id="nbr_tt"/>'
-  +'<span>내용</span><br><button type="button" style="background:#2D4996" id="nbr_img">사진첨부</button><textarea id="nbr_bd" style="max-width: 100vw; max-height: calc(100vh - 150px); width: 400px; height: 390px; display: block;"></textarea>'
+  +'<span>나무픽스를 이용하는도중 버그가발생하였나요?</span><br>'
+  +'<span>사진업로드는 imgur.com을 이용해주시길 바랍니다.</span>><br>'
+  +'<span style="color:red">별표 로 표시된것은 필수입력입니다.</span><br>'
+  +'<span style="color:green">TIP:닫기버튼을 눌러도 작성내용은 사라지지않습니다. 단, 인터넷브라우저가 닫히기 전까지.</span><br>'
+  +'<span>제목*</span><input name="title" type="text" style="width:100%" id="nbr_tt"/>'
+  +'<span>닉네임(이름)</span><input name="names" type="text" style="width:100%" id="nbr_name"/>'
+  +'<span>내용*</span><br><button type="button" style="background:#2D4996" id="nbr_img">사진첨부</button><textarea id="nbr_bd" style="max-width: 100vw; max-height: calc(100vh - 150px); width: 400px; height: 390px; display: block;"></textarea>'
   +'</div><div class="footer"><button type="button" style="background:#2D4996" id="nbr_close">닫기</button><button type="button" style="background:#2D4996" id="nbr_send">보내기</button></div></div></div>';
   $('.container-fluid.wiki-article').append(NEwindow_create);
   $('#nbr_close').click(function(event) {
@@ -46,11 +49,32 @@ function run() {
     /* Act on the event */
     var msg = "이미지주소를 입력하여주십시오. 예)http://i.imgur.com/0LINzxs.jpg";
     var result = prompt(msg);
-    if(result==""){
-      alert("이미지 주소가 없습니다.")
-    }else{
-      $('#nbr_bd').append("![image]("+result+")");
+    if(result===null){
+      return;
     }
+    if(result==""){
+          alert("이미지 주소가 없습니다.")
+        }else{
+          //$('#nbr_bd').append("![image]("+result+")");
+          var areaValue = $('#nbr_bd').val();
+          $('#nbr_bd').val(areaValue +"\n"+"![image]("+result+")");
+      }
+  });
+  var title = $("#nbr_tt").val();
+  var nick = $("#nbr_name").val();
+  var body = $("#nbr_bd").val();
+  $('#nbr_send').click(function(event) {
+    $.ajax({
+      url:"http://shironeko.nekopoly.n-e.kr/backend/respons.json",
+      type:"POST",
+      cache:false,
+      datatype:"json",
+      data:{"title":title,"body":body,"users":nick},
+      success: function(data) {
+            alert(data);
+            $('#nbr_ne_window').fadeOut(500, function() {/*Nothing*/});
+      },
+    });
   });
   $('#nbr_ne_window').hide(); //숨기기
  //-----------------------------------
